5b3c3d40c952d4a87a201c7028039967ab12bb28
Priyanka Pol <116531017+Priyanka-Pol@users.noreply.github.com>
2023-02-24 16:58:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 //your code here!
-var ol = document.getElementByID("infi-list");
-var li = document.createElement('li');
-li.textContent = todoValue.value;
-ol.appendChild(li);
-
+// var ol = document.getElementByID("infi-list");
+// var li = document.createElement('li');
+// li.textContent = todoValue.value;
+// ol.appendChild(li);
+$(window).on('scroll', function() {
+          if($(window).scrollTop() >= $('#infi-list').offset().top + $('#infi-list').outerHeight() - window.innerHeight) {
+            // alert('Bottom');
+			  ol.appendChild(li);
+			  ol.appendChild(li);
+          }
+        });
 
